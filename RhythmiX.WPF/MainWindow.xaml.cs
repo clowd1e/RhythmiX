86b64d86249597af30bbd2b1533a2f19cfec67d7
Олександр Лобченко <sashapro712@gmail.com>
2024-03-29 03:03:23 +0100

    Implementing basic window controll buttons

--- RhythmiX.WPF/MainWindow.xaml.cs
+++ RhythmiX.WPF/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RhythmiX.WPF
 {
@@ -19,6 +11,22 @@ namespace RhythmiX.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void HeaderBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
