b6039477e2462bb2eb020cb42d8a3ef811e8b7b0
Олександр Лобченко <sashapro712@gmail.com>
2024-04-02 20:21:26 +0200

    Adding components

---
+++ RhythmiX.WPF/Components/DragableField.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RhythmiX.WPF.Components
+{
+    /// <summary>
+    /// Interaction logic for DragableField.xaml
+    /// </summary>
+    public partial class DragableField : UserControl
+    {
+        public event RoutedEventHandler FieldDraged;
+        public DragableField()
+        {
+            InitializeComponent();
+        }
+
+        private void DragField_MouseDown(object sender, RoutedEventArgs e)
+        {
+            FieldDraged?.Invoke(this, e);
+        }
+    }
+}
---
+++ RhythmiX.WPF/Components/WindowControlButtons.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RhythmiX.WPF.Components
+{
+    /// <summary>
+    /// Interaction logic for WindowControlButtons.xaml
+    /// </summary>
+    public partial class WindowControlButtons : UserControl
+    {
+        public event RoutedEventHandler MinimizeClicked;
+
+        public WindowControlButtons()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            MinimizeClicked?.Invoke(this, e);
+        }
+    }
+}
