ecf52c0829ed3b357bc0645c16717855bcf37965
Олександр Лобченко <sashapro712@gmail.com>
2024-04-08 11:01:11 +0200

    Adding Sign up form

---
+++ RhythmiX.WPF/Views/SignupWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RhythmiX.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for SignupWindow.xaml
+    /// </summary>
+    public partial class SignupWindow : Window
+    {
+        public SignupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowControlButtons_MinimizeClicked(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void DragableField_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}
