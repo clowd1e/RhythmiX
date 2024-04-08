98a78274d4560b9d1fd82939b6f8ca6eef60cc83
Олександр Лобченко <sashapro712@gmail.com>
2024-04-08 11:03:19 +0200

    Adding command for changing main window to sin up form

--- RhythmiX.WPF/ViewModels/LoginWindowModel.cs
+++ RhythmiX.WPF/ViewModels/LoginWindowModel.cs
@@ -1,9 +1,14 @@
-﻿
+﻿using RhythmiX.WPF.Commands;
+using System.Windows.Input;
 
 namespace RhythmiX.WPF.ViewModels
 {
     public class LoginWindowModel : ViewModelBase
     {
-
+        public ICommand ChangeToSingupForm { get; }
+        public LoginWindowModel()
+        {
+            ChangeToSingupForm = new ChangeToSingupFormCommand();
+        }
     }
 }
