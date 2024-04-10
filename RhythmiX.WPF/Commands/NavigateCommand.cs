855d40f136bb4242073332b969790b4ddf125979
Олександр Лобченко <sashapro712@gmail.com>
2024-04-11 00:47:56 +0200

    Adding NavigateCommand

---
+++ RhythmiX.WPF/Commands/NavigateCommand.cs
@@ -0,0 +1,22 @@
+﻿using RhythmiX.WPF.Stores;
+using RhythmiX.WPF.ViewModels;
+
+namespace RhythmiX.WPF.Commands
+{
+    public class NavigateCommand : CommandBase
+    {
+        private readonly NavigationStore navigateStore;
+        private readonly ViewModelBase viewModel;
+
+        public NavigateCommand(NavigationStore navigationStore, ViewModelBase viewModel)
+        {
+            this.navigateStore = navigationStore;
+            this.viewModel = viewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            navigateStore.CurrentViewModel = viewModel;
+        }
+    }
+}
