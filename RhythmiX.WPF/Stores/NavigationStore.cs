6143896496871a4cd4e90bae72e969b5bfc38e48
Олександр Лобченко <sashapro712@gmail.com>
2024-04-11 00:48:12 +0200

    Implementing NavigationStore

--- RhythmiX.WPF/Stores/NavigationStore.cs
+++ RhythmiX.WPF/Stores/NavigationStore.cs
@@ -1,7 +1,25 @@
-﻿namespace RhythmiX.WPF.Stores
+﻿using RhythmiX.WPF.ViewModels;
+
+namespace RhythmiX.WPF.Stores
 {
     public class NavigationStore
     {
+        private ViewModelBase currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => currentViewModel;
+            set
+            {
+                currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
 
+        public event Action CurrentViewModelChanged;
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
     }
 }
