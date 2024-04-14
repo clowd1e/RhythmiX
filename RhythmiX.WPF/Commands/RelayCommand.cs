ccc0c66ef6904dfcc7f07d614663cff6be9f076b
Олександр Лобченко <sashapro712@gmail.com>
2024-04-14 19:43:00 +0200

    Adding RelayCommand

---
+++ RhythmiX.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace RhythmiX.WPF.Commands
+{
+    public class RelayCommand : CommandBase
+    {
+        private readonly Action<object> execute;
+
+        public RelayCommand(Action<object> execute)
+        {
+            this.execute = execute;
+        }
+
+        public override void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
