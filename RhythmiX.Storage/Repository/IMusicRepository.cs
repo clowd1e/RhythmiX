45fca43afcd47aa8d124c1883d1f0e675d6073ba
Олександр Лобченко <sashapro712@gmail.com>
2024-04-20 01:38:17 +0200

    Adding IMusicRepository

---
+++ RhythmiX.Storage/Repository/IMusicRepository.cs
@@ -0,0 +1,18 @@
+﻿using RhythmiX.Storage.Entities;
+using RhythmiX.Storage.Models;
+using RhythmiX.Storage.Models.Interfaces;
+
+namespace RhythmiX.Storage.Repository
+{
+    public interface IMusicRepository
+    {
+        Task<List<IHomeObservable>> GetUserHomePageContentAsync(long userId);
+        Task<List<TrackModel>> GetUserLikedTracksAsync(long userId);
+        Task<List<PlaylistModel>> GetUserLikedPlaylistsAsync(long userId);
+        Task<List<IHistoryObservable>> GetUserHistoryAsync(long userId);
+        Task<TrackModel> GetTrackByIdAsync(long trackId);
+        Task<PlaylistModel> GetPlaylistByIdAsync(long playlistId);
+        Task<ArtistModel> GetArtistByIdAsync(long artistId);
+        Task<AlbumModel> GetAlbumByIdAsync(long albumId);
+    }
+}
