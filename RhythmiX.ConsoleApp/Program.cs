4ab2ff58c4457336138e5fd54cf856fd2162ce56
Олександр Лобченко <sashapro712@gmail.com>
2024-04-21 02:02:13 +0200

    Testing API call

--- RhythmiX.ConsoleApp/Program.cs
+++ RhythmiX.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RhythmiX.Service.API;
 using RhythmiX.Service.API.ResponseModels;
+using RhythmiX.Service.TrackDownloader;
 
 namespace RhythmiX.ConsoleApp
 {
@@ -9,8 +10,12 @@ namespace RhythmiX.ConsoleApp
         {
             try
             {
-                TrackResponseModel? response = await ApiHelper.GetTrackByIdAsync(1532771);
-                Console.WriteLine(response?.Results[0].Name);
+                TrackResponseModel? response = await ApiHelper.GetObjectByIdAsync<TrackResponseModel>("tracks", 1532771, "&include=musicinfo");
+                Console.WriteLine(response.Results[0].Name);
+                await TrackDownloader.DownloadTrackAsync(response.Results[0].Name, response.Results[0].AudioDownload);
+
+                ArtistResponseModel? artistResponse = await ApiHelper.GetObjectByIdAsync<ArtistResponseModel>("artists", response.Results[0].ArtistId);
+                Console.WriteLine(artistResponse.Results[0].Name);
             }
             catch (HttpRequestException e)
             {
