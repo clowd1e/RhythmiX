e5bb9aa76159630721033cfee473b2ba01389ac4
Олександр Лобченко <sashapro712@gmail.com>
2024-04-23 16:44:15 +0200

    Testing API call for rewrote Downloader system

--- RhythmiX.ConsoleApp/Program.cs
+++ RhythmiX.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RhythmiX.Service.API;
 using RhythmiX.Service.API.ResponseModels;
-using RhythmiX.Service.TrackDownloader;
+using RhythmiX.Service.Downloaders.TrackDownloader;
+using RhythmiX.Service.Downloaders.ZipDownloader;
 
 namespace RhythmiX.ConsoleApp
 {
@@ -8,23 +9,12 @@ namespace RhythmiX.ConsoleApp
     {
         static async Task Main(string[] args)
         {
-            try
-            {
-                TrackResponseModel? response = await ApiHelper.GetObjectByIdAsync<TrackResponseModel>("tracks", 1532771, "&include=musicinfo");
+                TrackResponseModel? response = await ApiHelper.GetObjectByIdAsync<TrackResponseModel>("tracks", 1532772, "&include=musicinfo");
                 Console.WriteLine(response.Results[0].Name);
-                await TrackDownloader.DownloadTrackAsync(response.Results[0].Name, response.Results[0].AudioDownload);
+                await TrackDownloader.DownloadTrackAsync(response, "../../../../RhythmiX.WPF/APICallResults/DownloadedMusic");
 
                 ArtistResponseModel? artistResponse = await ApiHelper.GetObjectByIdAsync<ArtistResponseModel>("artists", response.Results[0].ArtistId);
                 Console.WriteLine(artistResponse.Results[0].Name);
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
         }
     }
 }
