dd889f482002121d65dbe16bf76ff113205d8e49
Олександр Лобченко <sashapro712@gmail.com>
2024-04-23 16:42:15 +0200

    Adding ZipDownloader

---
+++ RhythmiX.Service/Downloaders/ZipDownloader/ZipDownloader.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Compression;
+using System.Net;
+
+namespace RhythmiX.Service.Downloaders.ZipDownloader
+{
+    public static class ZipDownloader
+    {
+        public static async Task DownloadZipAsync(string name, string zipPath, string imagePath, string folderPath)
+        {
+            Directory.CreateDirectory($"{folderPath}/{name}");
+            Uri url = new Uri(zipPath);
+            string zipSavePath = $"{folderPath}/{name}/{name}.zip";
+
+            if (File.Exists(folderPath))
+                return;
+
+            using (WebClient client = new WebClient())
+            {
+                await client.DownloadFileTaskAsync(url, zipSavePath);
+                await ImageDownloader.ImageDownloader.DownoladImageAsync(name, imagePath, folderPath);
+            }
+
+            ZipFile.ExtractToDirectory(zipSavePath, folderPath);
+            File.Delete(zipSavePath);
+        }
+    }
+}
