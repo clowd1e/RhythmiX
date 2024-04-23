b490bc6dc9b1dfc9cd177a98674a7cfc82b74aaf
Олександр Лобченко <sashapro712@gmail.com>
2024-04-23 16:41:48 +0200

    Changing TrackDownloader's destination

---
+++ RhythmiX.Service/Downloaders/TrackDownloader/TrackDownloader.cs
@@ -0,0 +1,28 @@
+﻿using RhythmiX.Service.API.ResponseModels;
+using System.Net;
+
+namespace RhythmiX.Service.Downloaders.TrackDownloader
+{
+    public static class TrackDownloader
+    {
+        public static async Task DownloadTrackAsync(TrackResponseModel response, string folderPath)
+        {
+            string name = response.Results[0].Name;
+            string audioPath = response.Results[0].AudioDownload;
+            string imagePath = response.Results[0].AlbumImage;
+
+            Directory.CreateDirectory($"{folderPath}/{name}");
+            Uri url = new Uri(audioPath);
+            string trackSavePath = $"{folderPath}/{name}/{name}.mp3";
+
+            if (File.Exists(folderPath))
+                return;
+
+            using (WebClient client = new WebClient())
+            {
+                await client.DownloadFileTaskAsync(url, trackSavePath);
+                await ImageDownloader.ImageDownloader.DownoladImageAsync(name, imagePath, folderPath);
+            }
+        }
+    }
+}
