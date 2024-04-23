2d402fb7c8a7882232523366d3a68c6c6cfbac82
Олександр Лобченко <sashapro712@gmail.com>
2024-04-23 16:42:02 +0200

    Adding ImageDownloader

---
+++ RhythmiX.Service/Downloaders/ImageDownloader/ImageDownloader.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace RhythmiX.Service.Downloaders.ImageDownloader
+{
+    public static class ImageDownloader
+    {
+        public static async Task DownoladImageAsync(string name, string imagePath, string folderPath)
+        {
+            Uri url = new Uri(imagePath);
+            folderPath = $"{folderPath}/{name}/{name}.jpg";
+
+            if (File.Exists(folderPath))
+                return;
+
+            using (WebClient client = new WebClient())
+            {
+                await client.DownloadFileTaskAsync(url, folderPath);
+            }
+        }
+    }
+}
