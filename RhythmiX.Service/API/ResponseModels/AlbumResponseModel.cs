95ca9462d62afe5cf8e8590d1f97460679be1439
Олександр Лобченко <sashapro712@gmail.com>
2024-04-24 16:20:55 +0200

    Adding ResponseModels for Album and Playlist

---
+++ RhythmiX.Service/API/ResponseModels/AlbumResponseModel.cs
@@ -0,0 +1,9 @@
+﻿using RhythmiX.Storage.Models;
+
+namespace RhythmiX.Service.API.ResponseModels
+{
+    public class AlbumResponseModel : ResponseModelBase
+    {
+        public List<AlbumModel> Results { get; set; } 
+    }
+}
---
+++ RhythmiX.Service/API/ResponseModels/PlaylistResponseModel.cs
@@ -0,0 +1,9 @@
+﻿using RhythmiX.Storage.Models;
+
+namespace RhythmiX.Service.API.ResponseModels
+{
+    public class PlaylistResponseModel : ResponseModelBase
+    {
+        public List<PlaylistModel> Results { get; set; }
+    }
+}
