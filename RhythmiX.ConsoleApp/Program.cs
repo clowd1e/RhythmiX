289542771990ab64a7475ff89a725191de73d110
Олександр Лобченко <sashapro712@gmail.com>
2024-04-24 16:22:22 +0200

    Testing API calls

--- RhythmiX.ConsoleApp/Program.cs
+++ RhythmiX.ConsoleApp/Program.cs
@@ -9,12 +9,25 @@ namespace RhythmiX.ConsoleApp
     {
         static async Task Main(string[] args)
         {
-                TrackResponseModel? response = await ApiHelper.GetObjectByIdAsync<TrackResponseModel>("tracks", 1532772, "&include=musicinfo");
-                Console.WriteLine(response.Results[0].Name);
-                await TrackDownloader.DownloadTrackAsync(response, "../../../../RhythmiX.WPF/APICallResults/DownloadedMusic");
+            TrackResponseModel? response = await ApiHelper.GetObjectByIdAsync<TrackResponseModel>("tracks", 1532772, "&include=musicinfo");
+            Console.WriteLine(response.Results[0].Name);
+            await TrackDownloader.DownloadTrackAsync(response, "../../../../RhythmiX.WPF/APICallResults/DownloadedMusic");
 
-                ArtistResponseModel? artistResponse = await ApiHelper.GetObjectByIdAsync<ArtistResponseModel>("artists", response.Results[0].ArtistId);
-                Console.WriteLine(artistResponse.Results[0].Name);
+            ArtistResponseModel? artistResponse = await ApiHelper.GetObjectByIdAsync<ArtistResponseModel>("artists", response.Results[0].ArtistId);
+            Console.WriteLine(artistResponse.Results[0].Name);
+
+
+
+
+            AlbumResponseModel? albumResponse = await ApiHelper.GetObjectByIdAsync<AlbumResponseModel>("albums", 104336);
+            Console.WriteLine(albumResponse.Results[0].Name);
+            await ZipDownloader.DownloadAlbumAsync(albumResponse, "../../../../RhythmiX.WPF/APICallResults/DownloadedAlbums");
+
+
+
+            PlaylistResponseModel? playlistResponse = await ApiHelper.GetObjectByIdAsync<PlaylistResponseModel>("playlists", 218691);
+            Console.WriteLine(albumResponse.Results[0].Name);
+            await ZipDownloader.DownloadPlaylistAsync(playlistResponse, "../../../../RhythmiX.WPF/APICallResults/DownloadedPlaylists");
         }
     }
 }
