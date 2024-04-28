7769fb0df4e38a6f1aea759a9b69a5d67dcedb95
Олександр Лобченко <sashapro712@gmail.com>
2024-04-28 21:06:01 +0200

    Adding methods to add entities to user history

--- RhythmiX.Storage/Repository/IMusicRepository.cs
+++ RhythmiX.Storage/Repository/IMusicRepository.cs
@@ -27,6 +27,14 @@ namespace RhythmiX.Storage.Repository
         Task AddLikedArtistAsync(long userId, Artist artist);
         Task RemoveLikedArtistAsync(long userId, long artistId);
 
+
+
+        Task AddArtistToHistory(long userId, Artist artist);
+        Task AddTrackToHistory(long userId, Track track);
+        Task AddPlaylistToHistory(long userId, Playlist playlist);
+        Task AddAlbumToHistory(long userId, Album album);
+
+
         Task<bool> IsTrackLiked(long userId, long trackId);
         Task<bool> IsPlaylistLiked(long userId, long playlistId);
         Task<bool> IsAlbumLiked(long userId, long albumId);
