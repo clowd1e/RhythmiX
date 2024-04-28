e70db649729486de47ce74cffca6227111cc025e
Олександр Лобченко <sashapro712@gmail.com>
2024-04-28 22:19:05 +0200

    Adding Remove command for LikedArtist

---
+++ RhythmiX.Service/Command/LikedArtist/Remove/RemoveLikedArtistCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace RhythmiX.Service.Command.LikedArtist.Remove
+{
+    public class RemoveLikedArtistCommand : ICommand
+    {
+        public long UserId { get; set; }
+        public long ArtistId { get; set; }
+    }
+}
---
+++ RhythmiX.Service/Command/LikedArtist/Remove/RemoveLikedArtistCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Cinema.Service;
+using RhythmiX.Storage.Repository;
+
+namespace RhythmiX.Service.Command.LikedArtist.Remove
+{
+    public class RemoveLikedArtistCommandHandler : ICommandHandler<RemoveLikedArtistCommand>
+    {
+        private readonly IMusicRepository _repository;
+
+        public RemoveLikedArtistCommandHandler(IMusicRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result> HandleAsync(RemoveLikedArtistCommand command)
+        {
+            bool isArtistLiked = _repository.IsArtistLiked(command.UserId, command.ArtistId).Result;
+            if (!isArtistLiked)
+                return Result.Fail("Liked artist not found");
+
+            await _repository.RemoveLikedArtistAsync(command.UserId, command.ArtistId);
+
+            return Result.Ok();
+        }
+    }
+}
