3f06c7b6a0b7e0112bc0c2c80394191236bd1b11
Олександр Лобченко <sashapro712@gmail.com>
2024-04-28 15:35:24 +0200

    Adding Remove command for LikedTrack

---
+++ RhythmiX.Service/Command/LikedTrack/Remove/RemoveLikedTrackCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace RhythmiX.Service.Command.LikedTrack.Remove
+{
+    public class RemoveLikedTrackCommand : ICommand
+    {
+        public long UserId { get; set; }
+        public long TrackId { get; set; }
+    }
+}
---
+++ RhythmiX.Service/Command/LikedTrack/Remove/RemoveLikedTrackHandler.cs
@@ -0,0 +1,26 @@
+﻿using Cinema.Service;
+using RhythmiX.Storage.Repository;
+
+namespace RhythmiX.Service.Command.LikedTrack.Remove
+{
+    public class RemoveLikedTrackHandler : ICommandHandler<RemoveLikedTrackCommand>
+    {
+        private readonly IMusicRepository _repository;
+
+        public RemoveLikedTrackHandler(IMusicRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result> HandleAsync(RemoveLikedTrackCommand command)
+        {
+            bool isTrackLiked = await _repository.IsTrackLiked(command.UserId, command.TrackId);
+            if (!isTrackLiked)
+                return Result.Fail("Liked track not found.");
+
+            await _repository.RemoveLikedTrackAsync(command.UserId, command.UserId);
+
+            return Result.Ok();
+        }
+    }
+}
