f22ee3e162ec4d8ac597c5da0eaddf614c1345fa
Олександр Лобченко <sashapro712@gmail.com>
2024-04-28 21:07:35 +0200

    Adding and implementing remove command for LikedAlbum

---
+++ RhythmiX.Service/Command/LikedAlbums/Remove/RemoveLikedAlbumCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace RhythmiX.Service.Command.LikedAlbums.Remove
+{
+    public class RemoveLikedAlbumCommand : ICommand
+    {
+        public long UserId { get; set; }
+        public long AlbumId { get; set; }
+    }
+}
---
+++ RhythmiX.Service/Command/LikedAlbums/Remove/RemoveLikedAlbumCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Cinema.Service;
+using RhythmiX.Storage.Repository;
+
+namespace RhythmiX.Service.Command.LikedAlbums.Remove
+{
+    public class RemoveLikedAlbumCommandHandler : ICommandHandler<RemoveLikedAlbumCommand>
+    {
+        private readonly IMusicRepository _repository;
+        public RemoveLikedAlbumCommandHandler(IMusicRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result> HandleAsync(RemoveLikedAlbumCommand command)
+        {
+            bool isAlbumLiked = await _repository.IsAlbumLiked(command.UserId, command.AlbumId);
+            if (!isAlbumLiked)
+                return Result.Fail("Liked album not found.");
+
+            await _repository.RemoveLikedAlbumAsync(command.UserId, command.AlbumId);
+
+            return Result.Ok();
+        }
+    }
+}
