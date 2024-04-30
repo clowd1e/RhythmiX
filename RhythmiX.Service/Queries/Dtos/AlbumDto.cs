c43498b198f881ccf8c9468d26706a64b746e726
Олександр Лобченко <sashapro712@gmail.com>
2024-04-30 20:18:02 +0200

    Implementing AlbumDto

--- RhythmiX.Service/Queries/Dtos/AlbumDto.cs
+++ RhythmiX.Service/Queries/Dtos/AlbumDto.cs
@@ -2,6 +2,9 @@
 {
     public class AlbumDto
     {
-
+        public long Id { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+        public string Image { get; set; }
     }
 }
