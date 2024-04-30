23938dfdb12fcf7905e43096551bb83dea735d79
Олександр Лобченко <sashapro712@gmail.com>
2024-04-30 20:20:53 +0200

    Adding Recommended Albums for test

--- RhythmiX.WPF/ViewModels/MenuViewModels/HomeViewModel.cs
+++ RhythmiX.WPF/ViewModels/MenuViewModels/HomeViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace RhythmiX.WPF.ViewModels.MenuViewModels
+﻿using RhythmiX.Service.Queries.Dtos;
+using System.Collections.ObjectModel;
+
+namespace RhythmiX.WPF.ViewModels.MenuViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        private ObservableCollection<AlbumDto> albums = new ObservableCollection<AlbumDto>() { 
+            new AlbumDto() { AlbumName = "Album 1", ArtistName = "Artist 1", Image = "album_image_default.jpg" },
+            new AlbumDto() { AlbumName = "Album 2", ArtistName = "Artist 2", Image = "album_image_default.jpg" },
+            new AlbumDto() { AlbumName = "Album 3", ArtistName = "Artist 3", Image = "album_image_default.jpg" },
+            new AlbumDto() { AlbumName = "Album 4", ArtistName = "Artist 4", Image = "album_image_default.jpg" }
+        };
 
+        public ObservableCollection<AlbumDto> Albums
+        {
+            get => albums;
+        }
+
+        public HomeViewModel()
+        {
+
+        }
     }
 }
