a50dbc1b6a55fc735739ff2ea6ec9b96cc3ba79d
Олександр Лобченко <sashapro712@gmail.com>
2024-05-01 23:44:10 +0200

    Adding and implementing TrackLayout

---
+++ RhythmiX.WPF/Components/MusicComponents/TrackLayout.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace RhythmiX.WPF.Components.MusicComponents
+{
+    /// <summary>
+    /// Interaction logic for TrackLayout.xaml
+    /// </summary>
+    public partial class TrackLayout : UserControl
+    {
+        public TrackLayout()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TrackNameProperty =
+           DependencyProperty.Register("TrackName", typeof(string), typeof(TrackLayout), new PropertyMetadata("TrackName"));
+        public static readonly DependencyProperty TrackImageProperty =
+            DependencyProperty.Register("TrackImage", typeof(string), typeof(TrackLayout), new PropertyMetadata("track_image_default.png"));
+        public static readonly DependencyProperty TrackArtistProperty =
+            DependencyProperty.Register("TrackArtist", typeof(string), typeof(TrackLayout), new PropertyMetadata("TrackArtist"));
+
+        public string TrackName
+        {
+            get { return (string)GetValue(TrackNameProperty); }
+            set { SetValue(TrackNameProperty, value); }
+        }
+
+        public string TrackImage
+        {
+            get { return (string)GetValue(TrackImageProperty); }
+            set { SetValue(TrackImageProperty, value); }
+        }
+
+        public string TrackArtist
+        {
+            get { return (string)GetValue(TrackArtistProperty); }
+            set { SetValue(TrackArtistProperty, value); }
+        }
+
+        private void UpdateTrackName()
+        {
+            trackName.Text = TrackName;
+        }
+
+        private void UpdateTrackImage()
+        {
+            BitmapImage image = new BitmapImage(new Uri($"../../../APICallResults/DownloadedMusic/{TrackImage}", UriKind.Relative));
+            image.CacheOption = BitmapCacheOption.OnLoad;
+
+            trackImage.ImageSource = image;
+        }
+
+        private void UpdateTrackArtist()
+        {
+            artistName.Text = TrackArtist;
+        }
+
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateTrackName();
+            UpdateTrackImage();
+            UpdateTrackArtist();
+        }
+    }
+}
