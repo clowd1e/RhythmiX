030ee99af69e6d7859fd673d35038d06aa6bbb29
Олександр Лобченко <sashapro712@gmail.com>
2024-05-03 19:20:27 +0200

    Adding Login and Signup controllers

---
+++ RhythmiX.Web/Controllers/SignupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RhythmiX.Web.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
