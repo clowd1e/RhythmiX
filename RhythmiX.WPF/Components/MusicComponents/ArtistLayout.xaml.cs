336f7c4ed10e3cb45964e29fe7cc162dc534512c
Олександр Лобченко <sashapro712@gmail.com>
2024-05-05 22:51:08 +0200

    Adding and implementing ArtistLayout

---
+++ RhythmiX.WPF/Components/MusicComponents/ArtistLayout.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace RhythmiX.WPF.Components.MusicComponents
+{
+    /// <summary>
+    /// Interaction logic for ArtistLayout.xaml
+    /// </summary>
+    public partial class ArtistLayout : UserControl
+    {
+        public ArtistLayout()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ArtistNameProperty =
+            DependencyProperty.Register("ArtistName", typeof(string), typeof(ArtistLayout), new PropertyMetadata("ArtistName"));
+        public static readonly DependencyProperty ArtistImageProperty =
+            DependencyProperty.Register("ArtistImage", typeof(string), typeof(ArtistLayout), new PropertyMetadata("artist_image_default.png"));
+
+        public string ArtistName
+        {
+            get { return (string)GetValue(ArtistNameProperty); }
+            set { SetValue(ArtistNameProperty, value); }
+        }
+
+        public string ArtistImage
+        {
+            get { return (string)GetValue(ArtistImageProperty); }
+            set { SetValue(ArtistImageProperty, value); }
+        }
+
+        private void UpdateArtistName()
+        {
+            artistName.Text = ArtistName;
+        }
+
+        private void UpdateArtistImage()
+        {
+            BitmapImage image = new BitmapImage(new Uri($"../../../APICallResults/DownloadedArtists/{ArtistImage}", UriKind.Relative));
+            image.CacheOption = BitmapCacheOption.OnLoad;
+
+            artistImage.ImageSource = image;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateArtistName();
+            UpdateArtistImage();
+        }
+    }
+}
