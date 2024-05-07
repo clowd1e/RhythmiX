4ba84ba62973704313282a469e5682735ea6cefa
Олександр Лобченко <sashapro712@gmail.com>
2024-05-07 22:50:09 +0200

    Implementing PlaylistDto

--- RhythmiX.Service/Queries/Dtos/PlaylistDto.cs
+++ RhythmiX.Service/Queries/Dtos/PlaylistDto.cs
@@ -2,5 +2,9 @@
 {
     public class PlaylistDto
     {
+        public long Id { get; set; }
+        public string PlaylistName { get; set; }
+        public string ArtistName { get; set; }
+        public string Image { get; set; }
     }
 }
