7aa197cb61ca88c164ce7a8278765e1bb85c02e6
Олександр Лобченко <sashapro712@gmail.com>
2024-05-07 22:51:19 +0200

    Adding and implementing PlaylistsLayoutBox

---
+++ RhythmiX.WPF/Components/MusicComponents/PlaylistsLayoutBox.xaml.cs
@@ -0,0 +1,108 @@
+﻿using RhythmiX.Service.Queries.Dtos;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RhythmiX.WPF.Components.MusicComponents
+{
+    /// <summary>
+    /// Interaction logic for PlaylistsLayoutBox.xaml
+    /// </summary>
+    public partial class PlaylistsLayoutBox : UserControl
+    {
+        public PlaylistsLayoutBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ItemSourceProperty =
+            DependencyProperty.Register("ItemSource", typeof(ObservableCollection<PlaylistDto>), typeof(PlaylistsLayoutBox), new PropertyMetadata(null));
+        public static readonly DependencyProperty TitleProperty = 
+            DependencyProperty.Register("Title", typeof(string), typeof(PlaylistsLayoutBox), new PropertyMetadata("Playlists"));
+
+        public ObservableCollection<PlaylistDto> ItemSource
+        {
+            get { return (ObservableCollection<PlaylistDto>)GetValue(ItemSourceProperty); }
+            set { SetValue(ItemSourceProperty, value); }
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        private void UpdateItemSource()
+        {
+            listBox.ItemsSource = ItemSource;
+        }
+
+        private void UpdateTitle()
+        {
+            title.Text = Title;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateItemSource();
+            UpdateTitle();
+        }
+
+        private void listBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (listBox.Items.Count <= 0)
+                return;
+
+            if (listBox.Items.Count == 1)
+            {
+                var item = (ListBoxItem)listBox.ItemContainerGenerator.ContainerFromIndex(0);
+                item.Margin = new Thickness(0, 0, 0, 0);
+                return;
+            }
+
+            var firstItem = (ListBoxItem)listBox.ItemContainerGenerator.ContainerFromIndex(0);
+            if (firstItem is null)
+                return;
+
+            var lastItem = (ListBoxItem)listBox.ItemContainerGenerator.ContainerFromIndex(listBox.Items.Count - 1);
+            if (lastItem is null)
+                return;
+
+            firstItem.Margin = new Thickness(0, 0, 13, 0);
+            lastItem.Margin = new Thickness(13, 0, 0, 0);
+        }
+
+        private void ListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scrollViewer = GetParentScrollViewer((DependencyObject)sender);
+
+            if (scrollViewer != null)
+            {
+                if (e.Delta < 0)
+                {
+                    scrollViewer.LineDown();
+                }
+                else
+                {
+                    scrollViewer.LineUp();
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        private ScrollViewer GetParentScrollViewer(DependencyObject child)
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(child);
+
+            while (parent != null && !(parent is ScrollViewer))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as ScrollViewer;
+        }
+    }
+}
