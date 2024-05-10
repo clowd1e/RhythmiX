8bead2ec49613e7ea3dfb153c5bfe54e3a86187f
Олександр Лобченко <sashapro712@gmail.com>
2024-05-10 22:22:10 +0200

    Adding UserRepository

---
+++ RhythmiX.Storage/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using RhythmiX.Storage.Entities;
+
+namespace RhythmiX.Storage.Repository
+{
+    public interface IUserRepository
+    {
+        Task<bool> IsUsernameTakenAsync(string username);
+        Task AddUserAsync(User user);
+        Task<bool> IsUserExistsAsync(string username);
+        Task<bool> IsPasswordCorrectAsync(string username, string password);
+        Task LoginUserAsync(string username);
+    }
+}
---
+++ RhythmiX.Storage/Repository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using RhythmiX.Storage.Entities;
+
+namespace RhythmiX.Storage.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MusicDbContext _context;
+        public UserRepository(MusicDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsPasswordCorrectAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserExistsAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUsernameTakenAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LoginUserAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
