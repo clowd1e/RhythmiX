2d0bd4b75d738089cff621cc217d9f318f3fc55b
Олександр Лобченко <sashapro712@gmail.com>
2024-05-13 13:00:30 +0200

    Fix ChangeWindowCommand

--- RhythmiX.WPF/Commands/ChangeWindowCommand.cs
+++ RhythmiX.WPF/Commands/ChangeWindowCommand.cs
@@ -7,13 +7,20 @@ namespace RhythmiX.WPF.Commands
     {
         private readonly Window _window;
         private readonly Type _type;
-        private readonly Entity.User _user;
+        private readonly Entity.User? _user;
 
-        public ChangeWindowCommand(Window window, Type type, Entity.User user = null)
+        public ChangeWindowCommand(Window window, Type type, Entity.User user)
         {
             _window = window;
             _type = type;
             _user = user;
+        }
+
+        public ChangeWindowCommand(Window window, Type type)
+        {
+            _window = window;
+            _type = type;
+            _user = null;
         }
 
         public override void Execute(object? parameter)
@@ -23,7 +30,10 @@ namespace RhythmiX.WPF.Commands
 
         private void ChangeWindow()
         {
-            _window.DataContext = Activator.CreateInstance(_type, _user);
+            _window.DataContext = _user is null ? 
+                Activator.CreateInstance(_type) 
+                : Activator.CreateInstance(_type, _user);
+
             Application.Current.MainWindow.Close();
             Application.Current.MainWindow = _window;
             _window.Show();
