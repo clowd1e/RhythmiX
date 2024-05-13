c7acd0095327d81153b38de7006daed83a041650
Олександр Лобченко <sashapro712@gmail.com>
2024-05-13 12:51:27 +0200

    Add GetUser query

---
+++ RhythmiX.Service/Queries/User/GetUserQuery.cs
@@ -0,0 +1,8 @@
+﻿using Entity = RhythmiX.Storage.Entities;
+
+namespace RhythmiX.Service.Queries.User
+{
+    public sealed class GetUserQuery : IQuery<Entity.User>
+    {
+    }
+}
---
+++ RhythmiX.Service/Queries/User/GetUserQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using RhythmiX.Storage.Repository;
+using Entity = RhythmiX.Storage.Entities;
+
+namespace RhythmiX.Service.Queries.User
+{
+    public class GetUserQueryHandler : IQueryHandler<GetUserQuery, Entity.User>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly Entity.User _user;
+        public GetUserQueryHandler(IUserRepository userRepository, Entity.User user)
+        {
+            _userRepository = userRepository;
+            _user = user;
+        }
+        public async Task<Entity.User> HandleAsync(GetUserQuery query)
+        {
+            return await _userRepository.LoginUserAsync(_user.Username, _user.Password);
+        }
+    }
+}
