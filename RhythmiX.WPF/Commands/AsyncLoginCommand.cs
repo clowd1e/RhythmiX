cac0dd1c0dddc1a2f6797f69bc5db78810c06b4a
Олександр Лобченко <sashapro712@gmail.com>
2024-05-13 12:51:57 +0200

    Add AsyncLoginCommand

---
+++ RhythmiX.WPF/Commands/AsyncLoginCommand.cs
@@ -0,0 +1,55 @@
+﻿using Cinema.Service;
+using RhythmiX.Service.Command.User.Login;
+using RhythmiX.Service.Queries.User;
+using RhythmiX.Storage;
+using RhythmiX.Storage.Entities;
+using RhythmiX.Storage.Repository;
+using RhythmiX.WPF.ViewModels;
+using System.Windows.Input;
+
+namespace RhythmiX.WPF.Commands
+{
+    public class AsyncLoginCommand : AsyncCommandBase
+    {
+        private readonly User _userInitials;
+        private readonly LoginWindowModel _loginWindowModel;
+        public AsyncLoginCommand(User userInitials, LoginWindowModel loginWindowModel)
+        {
+            _userInitials = userInitials;
+            _loginWindowModel = loginWindowModel;
+        }
+
+        public async override Task ExecuteAsync(object? parameter)
+        {
+            using (MusicDbContext context = new MusicDbContext())
+            {
+                UserRepository userRepository = new UserRepository(context);
+
+                LoginUserCommand command = new LoginUserCommand(_userInitials.Username, _userInitials.Password);
+                LoginUserCommandHandler handler = new LoginUserCommandHandler(userRepository);
+
+                Result result = await handler.HandleAsync(command);
+                if (result.IsSuccess)
+                {
+                    GetUserQuery userQuery = new GetUserQuery();
+                    GetUserQueryHandler userQueryHandler = new GetUserQueryHandler(userRepository, _userInitials);
+                    User user = await userQueryHandler.HandleAsync(userQuery);
+
+                    ICommand changeWindowCommand = new ChangeWindowCommand(new MainWindow(), typeof(MainWindowModel), user);
+                    changeWindowCommand.Execute(null);
+                }
+                else
+                {
+                    
+                    Error? usernameError = result.Errors.FirstOrDefault(e => e.PropertyName == "Username");
+                    string usernameErrorMessage = usernameError?.Message;
+                    Error? passwordError = result.Errors.FirstOrDefault(e => e.PropertyName == "Password");
+                    string passwordErrorMessage = passwordError?.Message;
+
+                    _loginWindowModel.UsernameErrorMessage = usernameErrorMessage;
+                    _loginWindowModel.PasswordErrorMessage = passwordErrorMessage;
+                }
+            }
+        }
+    }
+}
