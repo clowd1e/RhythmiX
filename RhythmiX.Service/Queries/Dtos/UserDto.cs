dc1589775498524fbae3f0e9e3156cbb73b90b8f
Олександр Лобченко <sashapro712@gmail.com>
2024-05-15 00:27:05 +0200

    Add UserDto

---
+++ RhythmiX.Service/Queries/Dtos/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RhythmiX.Service.Queries.Dtos
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserDto() { }
+        public UserDto(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
