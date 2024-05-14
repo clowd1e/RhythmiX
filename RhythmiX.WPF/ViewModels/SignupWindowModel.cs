39f64d0ae099f6608bae89031538836704bc0992
Олександр Лобченко <sashapro712@gmail.com>
2024-05-14 23:30:46 +0200

    Add modal control to SignupWindow

--- RhythmiX.WPF/ViewModels/SignupWindowModel.cs
+++ RhythmiX.WPF/ViewModels/SignupWindowModel.cs
@@ -1,4 +1,5 @@
-﻿using RhythmiX.WPF.Commands;
+﻿using RhythmiX.Service.Queries.Dtos;
+using RhythmiX.WPF.Commands;
 using RhythmiX.WPF.Views;
 using System.Windows.Input;
 
@@ -6,10 +7,85 @@ namespace RhythmiX.WPF.ViewModels
 {
     public class SignupWindowModel : ViewModelBase
     {
-        public ICommand ChangeToLoginForm { get; }
+        private bool isModalOpen;
+        public bool IsModalOpen
+        {
+            get => isModalOpen;
+            set
+            {
+                isModalOpen = value;
+                OnPropertyChanged(nameof(IsModalOpen));
+            }
+        }
+
+        private string username;
+        public string Username
+        {
+            get => username;
+            set
+            {
+                username = value;
+                OnPropertyChanged(nameof(Username));
+                registerData.Username = value;
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+                registerData.Password = value;
+            }
+        }
+
+        private string repeatPassword;
+        public string RepeatPassword
+        {
+            get => repeatPassword;
+            set
+            {
+                repeatPassword = value;
+                OnPropertyChanged(nameof(RepeatPassword));
+                registerData.RepeatPassword = value;
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                OnPropertyChanged(nameof(Email));
+                registerData.Email = value;
+            }
+        }
+
+        private RegisterDto registerData;
+
+        public ICommand Signup { get; }
+        public ICommand Cancel { get; }
+        public ICommand Confirm { get; }
+
         public SignupWindowModel()
         {
-            ChangeToLoginForm = new ChangeWindowCommand(new LoginWindow(), typeof(LoginWindowModel));
+            registerData = new RegisterDto();
+            Signup = new RelayCommand(obj => IsModalOpen = true);
+
+            Cancel = new RelayCommand(obj => IsModalOpen = false);
+
+            Confirm = new AsyncRelayCommand(new Func<object, Task>(OnConfirm));
+        }
+
+        private async Task OnConfirm(object obj)
+        {
+            AsyncRegisterCommand registerCommand = new AsyncRegisterCommand(this, registerData);
+            await registerCommand.ExecuteAsync(obj);
         }
     }
 }
