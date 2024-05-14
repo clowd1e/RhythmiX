42ecc094059e15ca9cef6a048c47bb8eaca8949c
Олександр Лобченко <sashapro712@gmail.com>
2024-05-14 23:25:41 +0200

    Add RegisterDto

---
+++ RhythmiX.Service/Queries/Dtos/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿namespace RhythmiX.Service.Queries.Dtos
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+        public string Email { get; set; }
+
+        public RegisterDto() { }
+        public RegisterDto(string username, string password, string repeatPassword, string email)
+        {
+            Username = username;
+            Password = password;
+            RepeatPassword = repeatPassword;
+            Email = email;
+        }
+    }
+}
