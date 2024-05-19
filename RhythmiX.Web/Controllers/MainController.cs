81c4ff8454816c0ba21d295f6afeb504386def4a
Олександр Лобченко <sashapro712@gmail.com>
2024-05-19 23:45:01 +0200

    Add main views

---
+++ RhythmiX.Web/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RhythmiX.Web.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Explore()
+        {
+            return View();
+        }
+
+        public IActionResult Liked()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+
+        public IActionResult Playlists()
+        {
+            return View();
+        }
+
+        public IActionResult Account()
+        {
+            return View();
+        }
+    }
+}
