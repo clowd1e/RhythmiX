08b9085ce4adc0d3935fd43e00fedf70583e5ea2
Олександр Лобченко <sashapro712@gmail.com>
2024-05-20 19:00:01 +0200

    Add scroll.js

---
+++ RhythmiX.Web/wwwroot/js/scroll.js
@@ -0,0 +1,6 @@
+﻿function changeCss() {
+    let element = $("#search-bar");
+    this.scrollY > 100 ? element.css('background-color', '#090909') : element.css('background-color', '#171717');
+}
+
+window.addEventListener("scroll", changeCss, false);
