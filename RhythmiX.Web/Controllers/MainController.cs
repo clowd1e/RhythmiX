809d49497fdeafe42c854c2743d577c5db008efb
Олександр Лобченко <sashapro712@gmail.com>
2024-05-20 19:01:36 +0200

    Add static TrackDto list

--- RhythmiX.Web/Controllers/MainController.cs
+++ RhythmiX.Web/Controllers/MainController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RhythmiX.Service.Queries.Dtos;
 
 namespace RhythmiX.Web.Controllers
 {
@@ -16,7 +17,18 @@ namespace RhythmiX.Web.Controllers
 
         public IActionResult Liked()
         {
-            return View();
+            List<TrackDto> tracks = new List<TrackDto>()
+            {
+                new TrackDto() { Id = 1, TrackName = "Something", ArtistName = "Igor", Image = "track_image_default.jpg", Duration = "3:35" },
+                new TrackDto() { Id = 2, TrackName = "Really cool song", ArtistName = "NIRVANA", Image = "track_image_default.jpg", Duration = "5:14" },
+                new TrackDto() { Id = 3, TrackName = "I need a really long song name so that it is really long", ArtistName = "Long group name so that it is really long", Image = "track_image_default.jpg", Duration = "63:35" },
+                new TrackDto() { Id = 4, TrackName = "Something", ArtistName = "Igor", Image = "track_image_default.jpg", Duration = "3:35" },
+                new TrackDto() { Id = 5, TrackName = "Something", ArtistName = "Igor", Image = "track_image_default.jpg", Duration = "3:35" },
+                new TrackDto() { Id = 6, TrackName = "Something", ArtistName = "Igor", Image = "track_image_default.jpg", Duration = "3:35" },
+                new TrackDto() { Id = 7, TrackName = "Something", ArtistName = "Igor", Image = "track_image_default.jpg", Duration = "3:35" }
+            };
+
+            return View(tracks);
         }
 
         public IActionResult History()
