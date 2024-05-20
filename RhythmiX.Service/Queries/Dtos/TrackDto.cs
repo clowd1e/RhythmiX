e7dc1d04a88479adcc5c8445eb8d265dc4ad02e4
Олександр Лобченко <sashapro712@gmail.com>
2024-05-20 19:02:08 +0200

    Add Duration property and empty constructor

--- RhythmiX.Service/Queries/Dtos/TrackDto.cs
+++ RhythmiX.Service/Queries/Dtos/TrackDto.cs
@@ -4,10 +4,12 @@ namespace RhythmiX.Service.Queries.Dtos
 {
     public class TrackDto
     {
+        private const int SECONDS_IN_MINUTE = 60;
         public long Id { get; set; }
         public string TrackName { get; set; }
         public string ArtistName { get; set; }
         public string Image { get; set; }
+        public string Duration { get; set; }
 
         public TrackDto(TrackModel trackModel)
         {
@@ -15,6 +17,9 @@ namespace RhythmiX.Service.Queries.Dtos
             TrackName = trackModel.Name;
             ArtistName = trackModel.ArtistName;
             Image = $"{TrackName}.jpg";
+            Duration = $"{trackModel.Duration / SECONDS_IN_MINUTE}:{trackModel.Duration % SECONDS_IN_MINUTE}";
         }
+
+        public TrackDto() { }
     }
 }
