801f753d962c2d8c0847355809f365c8e8828f92
Олександр Лобченко <sashapro712@gmail.com>
2024-05-24 00:23:21 +0200

    Add db extension

---
+++ RhythmiX.Web/Extensions/MusicExtensions.cs
@@ -0,0 +1,16 @@
+﻿using RhythmiX.Storage;
+using RhythmiX.Storage.Repository;
+
+namespace RhythmiX.Web.Extensions
+{
+    public static class MusicExtensions
+    {
+        public static IServiceCollection AddMusicServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IMusicRepository, MusicRepository>();
+            serviceCollection.AddTransient<IUserRepository, UserRepository>();
+            serviceCollection.AddDbContext<MusicDbContext, MusicDbContext>();
+            return serviceCollection;
+        }
+    }
+}
