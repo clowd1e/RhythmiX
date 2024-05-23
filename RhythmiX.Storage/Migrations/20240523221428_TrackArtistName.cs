373b1675421f6bb2c468dccbf2663b3dea875110
Олександр Лобченко <sashapro712@gmail.com>
2024-05-24 00:23:01 +0200

    Add new migration

---
+++ RhythmiX.Storage/Migrations/20240523221428_TrackArtistName.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RhythmiX.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class TrackArtistName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ArtistName",
+                schema: "MusicDb",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArtistName",
+                schema: "MusicDb",
+                table: "Tracks");
+        }
+    }
+}
