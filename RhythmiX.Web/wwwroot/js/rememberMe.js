fcbcca6fe816846e2ac8281766f7464b4ed8f98e
Олександр Лобченко <sashapro712@gmail.com>
2024-05-25 00:44:24 +0200

    Add script for remember me feature

---
+++ RhythmiX.Web/wwwroot/js/rememberMe.js
@@ -0,0 +1,26 @@
+﻿function loadUserCredentials() {
+    var user = localStorage.getItem("user");
+    var password = localStorage.getItem("password");
+    if (!!user && !!password) {
+        $("#username").val(user);
+        $("#password").val(password);
+        $("#remember").prop("checked", true);
+    }
+}
+
+function saveUserCredentials() {
+    if ($("#remember").prop("checked") == true) {
+        var user = $("#username").val();
+        var password = $("#password").val();
+        localStorage.setItem("user", user);
+        localStorage.setItem("password", password);
+    }
+    else {
+        localStorage.removeItem("user");
+        localStorage.removeItem("password");
+    }
+}
+
+window.addEventListener("load", loadUserCredentials, false);
+
+$("#login").on("click", saveUserCredentials);
