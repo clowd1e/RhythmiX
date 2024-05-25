d78f64ed2c70006de7df0ff9aee76d5654a50c10
Олександр Лобченко <sashapro712@gmail.com>
2024-05-25 21:25:07 +0200

    Add MusicGroupStore

---
+++ RhythmiX.WPF/Stores/MusicGroupStore.cs
@@ -0,0 +1,16 @@
+﻿using RhythmiX.Service.API.Models.Interfaces;
+
+namespace RhythmiX.WPF.Stores
+{
+    public class MusicGroupStore<THomeObservable>
+        where THomeObservable : IHomeObservable
+    {
+        public string Title { get; set; }
+        public IEnumerable<THomeObservable> Items { get; set; }
+        public MusicGroupStore(string title, IEnumerable<THomeObservable> items)
+        {
+            Title = title;
+            Items = items;
+        }
+    }
+}
