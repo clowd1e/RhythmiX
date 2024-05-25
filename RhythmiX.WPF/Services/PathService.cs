03bcf43acdcbe27f7a1bb8a31ee1b05f4522cca4
Олександр Лобченко <sashapro712@gmail.com>
2024-05-25 21:25:30 +0200

    Add PathService

---
+++ RhythmiX.WPF/Services/PathService.cs
@@ -0,0 +1,11 @@
+﻿namespace RhythmiX.WPF.Services
+{
+    public static class PathService
+    {
+        public const string DownloadedTracksPath = "../../../APICallResults/DownloadedTracks";
+        public const string TrackDefaultImagePath = "../../../APICallResults/DownloadedTracks/track_image_default.jpg";
+        public const string DownloadedAlbumsPath = "../../../APICallResults/DownloadedAlbums";
+        public const string DownloadedArtistsPath = "../../../APICallResults/DownloadedArtists";
+        public const string DownloadedPlaylistsPath = "../../../APICallResults/DownloadedPlaylists";
+    }
+}
