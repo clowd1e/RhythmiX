bba28911b3fbdd68863c3f2726b3cb09e2ebbfc5
Олександр Лобченко <sashapro712@gmail.com>
2024-05-25 21:23:10 +0200

    Add InverseBooleanToVisibilityConverter

---
+++ RhythmiX.WPF/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RhythmiX.WPF.Converters
+{
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool b && b ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
