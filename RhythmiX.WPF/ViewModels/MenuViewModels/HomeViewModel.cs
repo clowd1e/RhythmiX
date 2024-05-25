67fbe8f6e87c1bed13b7b9afd0dc8afdb2aeaa0e
Олександр Лобченко <sashapro712@gmail.com>
2024-05-25 21:28:01 +0200

    Implement HomeViewModel

--- RhythmiX.WPF/ViewModels/MenuViewModels/HomeViewModel.cs
+++ RhythmiX.WPF/ViewModels/MenuViewModels/HomeViewModel.cs
@@ -1,27 +1,77 @@
-﻿using RhythmiX.Service.Queries.Dtos;
-using RhythmiX.Storage.Models;
-using RhythmiX.Storage.Models.Interfaces;
+﻿using RhythmiX.Service.API.Models;
+using RhythmiX.Service.API.Models.Interfaces;
+using RhythmiX.Service.Queries.Dtos;
 using RhythmiX.WPF.Commands;
 using RhythmiX.WPF.Stores;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace RhythmiX.WPF.ViewModels.MenuViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        private List<MusicGroupStore<IHomeObservable>> _content;
 
-        private List<IHomeObservable> _content;
 
-        private List<TrackModel> tracks;
-        private ObservableCollection<TrackDto> trackDtos;
-        public ObservableCollection<TrackDto> TrackDtos
+        private List<MusicGroupStore<TrackModel>> tracks;
+        private List<MusicGroupStore<AlbumModel>> albums;
+        private List<MusicGroupStore<ArtistModel>> artists;
+        private List<MusicGroupStore<PlaylistModel>> playlists;
+
+
+        private ObservableCollection<MusicGroupStore<TrackDto>> trackDtos;
+        public ObservableCollection<MusicGroupStore<TrackDto>> TrackDtos
         {
             get => trackDtos;
             set
             {
                 trackDtos = value;
                 OnPropertyChanged(nameof(trackDtos));
+            }
+        }
+
+        private ObservableCollection<MusicGroupStore<AlbumDto>> albumDtos;
+        public ObservableCollection<MusicGroupStore<AlbumDto>> AlbumDtos
+        {
+            get => albumDtos;
+            set
+            {
+                albumDtos = value;
+                OnPropertyChanged(nameof(albumDtos));
+            }
+        }
+
+        private ObservableCollection<MusicGroupStore<ArtistDto>> artistDtos;
+        public ObservableCollection<MusicGroupStore<ArtistDto>> ArtistDtos
+        {
+            get => artistDtos;
+            set
+            {
+                artistDtos = value;
+                OnPropertyChanged(nameof(artistDtos));
+            }
+        }
+
+        private ObservableCollection<MusicGroupStore<PlaylistDto>> playlistDtos;
+        public ObservableCollection<MusicGroupStore<PlaylistDto>> PlaylistDtos
+        {
+            get => playlistDtos;
+            set
+            {
+                playlistDtos = value;
+                OnPropertyChanged(nameof(playlistDtos));
+            }
+        }
+
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get => isLoading;
+            set
+            {
+                isLoading = value;
+                OnPropertyChanged(nameof(IsLoading));
             }
         }
 
@@ -34,68 +84,42 @@ namespace RhythmiX.WPF.ViewModels.MenuViewModels
             LoadContentCommand.Execute(null);
         }
 
-        public void UpdateContent(List<IHomeObservable> content)
+        public void UpdateContent(List<MusicGroupStore<IHomeObservable>> content)
         {
             _content = content;
-            tracks = _content.OfType<TrackModel>().ToList();
-            TrackDtos = new ObservableCollection<TrackDto>(tracks.Select(t => new TrackDto(t)).Take(9));
+            tracks = _content.Select(group => new MusicGroupStore<TrackModel>(
+                    group.Title, group.Items.OfType<TrackModel>().ToList()
+                    )).Where(group => group.Items.Any()).ToList();
+            albums = _content.Select(group => new MusicGroupStore<AlbumModel>(
+                    group.Title, group.Items.OfType<AlbumModel>().ToList()
+                    )).Where(group => group.Items.Any()).ToList();
+            artists = _content.Select(group => new MusicGroupStore<ArtistModel>(
+                    group.Title, group.Items.OfType<ArtistModel>().ToList()
+                    )).Where(group => group.Items.Any()).ToList();
+
+            playlists = _content.Select(group => new MusicGroupStore<PlaylistModel>(
+                    group.Title, group.Items.OfType<PlaylistModel>().ToList()
+                    )).Where(group => group.Items.Any()).ToList();
+
+            TrackDtos = new ObservableCollection<MusicGroupStore<TrackDto>>(
+                tracks.Select(group => new MusicGroupStore<TrackDto>(group.Title, new ObservableCollection<TrackDto>(group.Items
+                .Select(item => new TrackDto(item)))
+                )));
+
+            AlbumDtos = new ObservableCollection<MusicGroupStore<AlbumDto>>(
+                albums.Select(group => new MusicGroupStore<AlbumDto>(group.Title, new ObservableCollection<AlbumDto>(group.Items
+                .Select(item => new AlbumDto(item)))
+                )));
+
+            ArtistDtos = new ObservableCollection<MusicGroupStore<ArtistDto>>(
+                artists.Select(group => new MusicGroupStore<ArtistDto>(group.Title, new ObservableCollection<ArtistDto>(group.Items
+                .Select(item => new ArtistDto(item)))
+                )));
+
+            PlaylistDtos = new ObservableCollection<MusicGroupStore<PlaylistDto>>(
+                playlists.Select(group => new MusicGroupStore<PlaylistDto>(group.Title, new ObservableCollection<PlaylistDto>(group.Items
+                .Select(item => new PlaylistDto(item)))
+                )));
         }
-
-        //private ObservableCollection<AlbumDto> albums = new ObservableCollection<AlbumDto>() { 
-        //    new AlbumDto() { AlbumName = "Album 1", ArtistName = "Artist 1", Image = "album_image_default.jpg" },
-        //    new AlbumDto() { AlbumName = "Album 2", ArtistName = "Artist 2", Image = "album_image_default.jpg" },
-        //    new AlbumDto() { AlbumName = "Album 3", ArtistName = "Artist 3", Image = "album_image_default.jpg" },
-        //    new AlbumDto() { AlbumName = "Album 4", ArtistName = "Artist 4", Image = "album_image_default.jpg" }
-        //};
-
-        //private ObservableCollection<TrackDto> tracks = new ObservableCollection<TrackDto>()
-        //{
-        //    new TrackDto() { TrackName = "Track 1", ArtistName = "Artist 1", Image = "track_image_default.jpg" },
-        //    new TrackDto() { TrackName = "Track 2", ArtistName = "Artist 2", Image = "track_image_default.jpg" },
-        //    new TrackDto() { TrackName = "Track 3", ArtistName = "Artist 3", Image = "track_image_default.jpg" },
-        //    new TrackDto() { TrackName = "Really cool song", ArtistName = "NIRVANA", Image = "Nirvana.jpg" },
-        //    new TrackDto() { TrackName = "Track 5", ArtistName = "Artist 5", Image = "track_image_default.jpg" },
-        //    new TrackDto() { TrackName = "Track 6", ArtistName = "Artist 6", Image = "track_image_default.jpg" },
-        //    new TrackDto() { TrackName = "I need really long song name", ArtistName = "Long group name so that it's really long", Image = "NWA.jpg" },
-        //    new TrackDto() { TrackName = "Track 8", ArtistName = "Artist 8", Image = "track_image_default.jpg" },
-        //    new TrackDto() { TrackName = "Track 9", ArtistName = "Artist 9", Image = "track_image_default.jpg" }
-        //};
-
-        //private ObservableCollection<ArtistDto> artists = new ObservableCollection<ArtistDto>()
-        //{
-        //    new ArtistDto() { ArtistName = "NEFFEX", Image = "Neffex.jpg" },
-        //    new ArtistDto() { ArtistName = "Metallica", Image = "Metallica.jpg" },
-        //    new ArtistDto() { ArtistName = "AC/DC", Image = "ACDC.jpg" },
-        //    new ArtistDto() { ArtistName = "NEFFEX", Image = "Neffex.jpg" }
-        //};
-
-        //private ObservableCollection<PlaylistDto> playlists = new ObservableCollection<PlaylistDto>()
-        //{
-        //    new PlaylistDto() { PlaylistName = "Playlist 1", ArtistName = "User 1", Image = "playlist_image_default.png" },
-        //    new PlaylistDto() { PlaylistName = "Playlist 2", ArtistName = "Some other user", Image = "playlist_image_default.png" },
-        //    new PlaylistDto() { PlaylistName = "Playlist 3", ArtistName = "Probably me", Image = "playlist_image_default.png" },
-        //    new PlaylistDto() { PlaylistName = "Playlist 4", ArtistName = "YEAH", Image = "playlist_image_default.png" }
-        //};
-
-
-        //public ObservableCollection<AlbumDto> Albums
-        //{
-        //    get => albums;
-        //}
-
-        //public ObservableCollection<TrackDto> Tracks
-        //{
-        //    get => tracks;
-        //}
-
-        //public ObservableCollection<ArtistDto> Artists
-        //{
-        //    get => artists;
-        //}
-
-        //public ObservableCollection<PlaylistDto> Playlists
-        //{
-        //    get => playlists;
-        //}
     }
 }
