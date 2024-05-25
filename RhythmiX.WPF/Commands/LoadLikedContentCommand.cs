779bcba3c8e4c37457bf99a75d55ee48efbe085e
Олександр Лобченко <sashapro712@gmail.com>
2024-05-26 00:32:31 +0200

    Add and implement LoadLikedContentCommand

---
+++ RhythmiX.WPF/Commands/LoadLikedContentCommand.cs
@@ -0,0 +1,41 @@
+﻿using RhythmiX.Service.Downloaders.ImageDownloader;
+using RhythmiX.Service.Queries.LikedTrack;
+using RhythmiX.Storage;
+using RhythmiX.Storage.Entities;
+using RhythmiX.Storage.Repository;
+using RhythmiX.WPF.Services;
+using RhythmiX.WPF.ViewModels.MenuViewModels;
+
+namespace RhythmiX.WPF.Commands
+{
+    public class LoadLikedContentCommand : AsyncCommandBase
+    {
+        private readonly LikedViewModel _viewModel;
+        private readonly User _user;
+
+        public LoadLikedContentCommand(LikedViewModel viewModel, User user)
+        {
+            _viewModel = viewModel;
+            _user = user;
+        }
+
+        public async override Task ExecuteAsync(object? parameter)
+        {
+            _viewModel.IsLoading = true;
+
+            using MusicDbContext context = new MusicDbContext();
+            MusicRepository repository = new MusicRepository(context);
+
+            GetUserLikedTracksQuery query = new GetUserLikedTracksQuery();
+            GetUserLikedTracksQueryHandler handler = new GetUserLikedTracksQueryHandler(repository, _user);
+
+            IEnumerable<Track> likedTracks = await handler.HandleAsync(query);
+
+            await ImageDownloader.DownloadImagesAsync(likedTracks, PathService.DownloadedTracksPath);
+
+            _viewModel.UpdateContent(likedTracks);
+
+            _viewModel.IsLoading = false;
+        }
+    }
+}
