0775ca297b400cee81b79f0bf75f5c28abb1381a
Олександр Лобченко <sashapro712@gmail.com>
2024-05-27 23:30:36 +0200

    Add LoadMusicCommand

---
+++ RhythmiX.WPF/Commands/AsyncLoadMusicCommand.cs
@@ -0,0 +1,37 @@
+﻿using RhythmiX.Service.Downloaders.TrackDownloader;
+using RhythmiX.Storage.Entities;
+using RhythmiX.WPF.Services;
+using RhythmiX.WPF.Stores;
+using RhythmiX.WPF.ViewModels;
+
+namespace RhythmiX.WPF.Commands
+{
+    public class AsyncLoadMusicCommand : AsyncCommandBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly List<Track> _tracks;
+
+        public AsyncLoadMusicCommand(NavigationStore navigationStore, List<Track> tracks)
+        {
+            _tracks = tracks;
+            _navigationStore = navigationStore;
+        }
+
+        public async override Task ExecuteAsync(object? parameter)
+        {
+            MusicControlViewModel musicControl = new MusicControlViewModel();
+            _navigationStore.CurrentViewModel = musicControl;
+
+            musicControl.IsLoading = true;
+
+            await DownloadMusicFiles();
+
+            musicControl.IsLoading = false;
+        }
+
+        private async Task DownloadMusicFiles()
+        {
+            await TrackDownloader.DownloadTracksAsync(_tracks, PathService.DownloadedTracksPath);
+        }
+    }
+}
