ee5958fa62bb085e78c2058d22f924b6f9ef200a
Олександр Лобченко <sashapro712@gmail.com>
2024-05-27 23:32:49 +0200

    Add MusicControlViewModel

---
+++ RhythmiX.WPF/ViewModels/MusicControlViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace RhythmiX.WPF.ViewModels
+{
+    public class MusicControlViewModel : ViewModelBase
+    {
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get => isLoading;
+            set
+            {
+                isLoading = value;
+                OnPropertyChanged(nameof(IsLoading));
+            }
+        }
+    }
+}
