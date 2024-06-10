4ba1cf844e73d8dae20c12e6079ef396d3f4b500
Олександр Лобченко <sashapro712@gmail.com>
2024-06-10 23:28:51 +0200

    Update GetUserLikedTracksQueryHandler

--- RhythmiX.Service/Queries/LikedTrack/GetUserLikedTracksQueryHandler.cs
+++ RhythmiX.Service/Queries/LikedTrack/GetUserLikedTracksQueryHandler.cs
@@ -7,15 +7,22 @@ namespace RhythmiX.Service.Queries.LikedTrack
     public class GetUserLikedTracksQueryHandler : IQueryHandler<GetUserLikedTracksQuery, IEnumerable<Track>>
     {
         private readonly IMusicRepository _musicRepository;
-        private readonly Entities.User _user;
+        private readonly long _userId;
         public GetUserLikedTracksQueryHandler(IMusicRepository musicRepository, Entities.User user)
         {
             _musicRepository = musicRepository;
-            _user = user;
+            _userId = user.Id;
         }
+
+        public GetUserLikedTracksQueryHandler(IMusicRepository musicRepository, long userId)
+        {
+            _musicRepository = musicRepository;
+            _userId = userId;
+        }
+
         public async Task<IEnumerable<Track>> HandleAsync(GetUserLikedTracksQuery query)
         {
-            return await _musicRepository.GetUserLikedTracksAsync(_user.Id);
+            return await _musicRepository.GetUserLikedTracksAsync(_userId);
         }
     }
 }
