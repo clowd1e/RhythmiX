d69ccc01231030e3982524d89aef3b0655a80522
Олександр Лобченко <sashapro712@gmail.com>
2024-06-12 23:09:29 +0200

    Add url table row script

---
+++ RhythmiX.Web/wwwroot/js/trOnClick.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    let rows = document.querySelectorAll(".clickable-row");
+    rows.forEach(function (row) {
+        row.addEventListener("click", function () {
+            let url = this.dataset.url;
+            window.location.href = url;
+        });
+    });
+});
