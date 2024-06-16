09c1ea60fb9445a791bb3412346e77bcbe0a814b
Олександр Лобченко <sashapro712@gmail.com>
2024-06-16 16:05:37 +0200

    Add UserService to store User credentials

---
+++ RhythmiX.Web/Services/UserService/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using RhythmiX.Storage.Entities;
+
+namespace RhythmiX.Web.Services.UserService
+{
+    public interface IUserService
+    {
+        long Id { get; set; }
+        string Username { get; set; }
+        string Password { get; set; }
+        void SetUser(User user);
+    }
+}
---
+++ RhythmiX.Web/Services/UserService/UserService.cs
@@ -0,0 +1,17 @@
+﻿using RhythmiX.Storage.Entities;
+
+namespace RhythmiX.Web.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public void SetUser(User user)
+        {
+            Id = user.Id;
+            Username = user.Username;
+            Password = user.Password;
+        }
+    }
+}
