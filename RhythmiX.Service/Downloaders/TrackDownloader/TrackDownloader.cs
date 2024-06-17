bb8ec50f44ed62b2ae364ba52770f57864961f44
Олександр Лобченко <sashapro712@gmail.com>
2024-06-17 18:22:06 +0200

    Add DownloadTrackAsync with Track parameter

--- RhythmiX.Service/Downloaders/TrackDownloader/TrackDownloader.cs
+++ RhythmiX.Service/Downloaders/TrackDownloader/TrackDownloader.cs
@@ -27,13 +27,31 @@ namespace RhythmiX.Service.Downloaders.TrackDownloader
             }
         }
 
+        public static async Task DownloadTrackAsync(Track track, string folderPath)
+        {
+            string name = track.Name;
+            string audioPath = track.AudioDownload;
+
+            Directory.CreateDirectory($"{folderPath}/{name}");
+            Uri url = new Uri(audioPath);
+            string trackSavePath = $"{folderPath}/{name}/{name}{audioFormat}";
+
+            if (File.Exists($"{folderPath}/{name}/{name}{audioFormat}"))
+                return;
+
+            using (WebClient client = new WebClient())
+            {
+                await client.DownloadFileTaskAsync(url, trackSavePath);
+            }
+        }
+
         public static async Task DownloadTracksAsync(TrackResponseModel response, string folderPath)
         {
             foreach (var track in response.Results)
             {
                 string name = track.Name;
                 string audioPath = track.AudioDownload;
-                string imagePath = track.AlbumImage;
+                string imagePath = string.IsNullOrEmpty(track.Image) ? track.AlbumImage : track.Image;
 
                 Directory.CreateDirectory($"{folderPath}/{name}");
                 Uri url = new Uri(audioPath);
@@ -56,7 +74,6 @@ namespace RhythmiX.Service.Downloaders.TrackDownloader
             {
                 string name = track.Name;
                 string audioPath = track.AudioDownload;
-                string imagePath = track.AlbumImage;
 
                 Directory.CreateDirectory($"{folderPath}/{name}");
                 Uri url = new Uri(audioPath);
