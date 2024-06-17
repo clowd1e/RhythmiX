89f2a7d58b18bac50956531c0fad4be62377bc9c
Олександр Лобченко <sashapro712@gmail.com>
2024-06-17 18:22:28 +0200

    Add PathService

---
+++ RhythmiX.Web/Services/PathService.cs
@@ -0,0 +1,8 @@
+﻿namespace RhythmiX.Web.Services
+{
+    public static class PathService
+    {
+        public const string DownloadedTracksPath = "wwwroot/APICallResults/DownloadedTracks";
+        public const string DownloadedTracksRazorPath = "~/APICallResults/DownloadedTracks";
+    }
+}
