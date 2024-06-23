5cd8e7bfba8a91e60a89d350506b2ff6020b933c
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-06-23 22:30:25 +0200

    Add AddHistoryTrackCommand

---
+++ RhythmiX.Service/Command/HistoryTrack/Add/AddHistoryTrackCommand.cs
@@ -0,0 +1,15 @@
+﻿using RhythmiX.Storage.Entities;
+
+namespace RhythmiX.Service.Command.HistoryTrack.Add
+{
+    public class AddHistoryTrackCommand : ICommand
+    {
+        public long UserId { get; set; }
+        public Track Track { get; set; }
+        public AddHistoryTrackCommand(long userId, Track track)
+        {
+            UserId = userId;
+            Track = track;
+        }
+    }
+}
---
+++ RhythmiX.Service/Command/HistoryTrack/Add/AddHistoryTrackCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Cinema.Service;
+using RhythmiX.Storage.Repository;
+
+namespace RhythmiX.Service.Command.HistoryTrack.Add
+{
+    public class AddHistoryTrackCommandHandler : ICommandHandler<AddHistoryTrackCommand>
+    {
+        private readonly IMusicRepository _repository;
+        public AddHistoryTrackCommandHandler(IMusicRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result> HandleAsync(AddHistoryTrackCommand command)
+        {
+            await _repository.AddTrackToHistoryAsync(command.UserId, command.Track);
+
+            return Result.Ok();
+        }
+    }
+}
